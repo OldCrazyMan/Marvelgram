0131c9f4feb465b7bfea55017bc6dc103993c6c5
Tim Akhm <akhmtimyr@gmail.com>
2024-01-26 15:14:34 +0200

    prepare for MVVM-build

---
+++ Marvelgram/Application/ModelBuilder.swift
@@ -0,0 +1,8 @@
+//
+//  ModelBuilder.swift
+//  Marvelgram
+//
+//  Created by Tim Akhmetov on 26.01.2024.
+//
+
+import Foundation
---
+++ Marvelgram/Models/MovieTableCellViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MovieTableCellViewModel.swift
+//  Marvelgram
+//
+//  Created by Tim Akhmetov on 26.01.2024.
+//
+
+import Foundation
